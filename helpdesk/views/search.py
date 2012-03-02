eede40dd2bc277a0d1af55d2f1a54e9f620fb7bb
tschmidt <tschmidt@sacfoodcoop.com>
2012-03-02 09:50:41 -0800

    added AGPLv3 license

--- helpdesk/views/search.py
+++ helpdesk/views/search.py
@@ -1,9 +1,20 @@
 """
 django-pm - A ticketing and project management tool.
 
-(c) Copyright 2012 Sacramento Natural Foods Co-op, Inc. All Rights Reserved. See LICENSE for details.
+Copyright (C) 2012 Sacramento Natural Foods Co-op, Inc.
 
-views/search.py - autocomplete function for Haystack search. 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from helpdesk.models import Ticket, Queue
