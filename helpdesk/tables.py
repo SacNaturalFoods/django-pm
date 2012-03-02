6d6d07938291aa7549989f132b674a6cd83b341d
tschmidt <tschmidt@sacfoodcoop.com>
2012-03-02 10:42:36 -0800

    added ticket search links

--- helpdesk/tables.py
+++ helpdesk/tables.py
@@ -7,13 +7,17 @@ tables.py - django-tables2 table classes
 """
 
 import django_tables2 as tables
+from django_tables2.utils import A
 
 class TicketTable(tables.Table):
     id = tables.Column()
     priority = tables.Column()
-    title = tables.Column()
+    title = tables.LinkColumn('helpdesk_view', args=[A('id')])
     queue = tables.Column()
     status = tables.Column()
+    
+    #def render_title(self, value):
+    #    return '<a href="%s">%s</a>' % (value.get_absolute_url, value.title)
 
     class Meta:
         attrs = {'class': 'paleblue'}
