2b6ba60f371b06bda214b2841f36373f97d591c5
tschmidt-dev <tschmidt@sacfoodcoop.com>
2012-05-18 10:17:44 -0700

    removed extra breaks added to ticket update email - no longer necessary due to wysiwyg editor

--- helpdesk/lib.py
+++ helpdesk/lib.py
@@ -90,14 +90,7 @@ def send_templated_mail(template_name, email_context, recipients, sender=None, b
 
     email_html_base_file = os.path.join('helpdesk', locale, 'email_html_base.html')
 
-
-    ''' keep new lines in html emails '''
-    from django.utils.safestring import mark_safe
-
-    if context.has_key('comment'):
-        html_txt = context['comment']
-        html_txt = html_txt.replace('\r\n', '<br>') if html_txt else None
-        context['comment'] = mark_safe(html_txt)
+   ['comment'] = mark_safe(html_txt)
 
     html_part = loader.get_template_from_string(
         "{%% extends '%s' %%}{%% block title %%}%s{%% endblock %%}{%% block content %%}%s{%% endblock %%}" % (email_html_base_file, t.heading, t.html)
