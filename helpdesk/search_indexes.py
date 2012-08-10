88f919892cd6c8cfb8bbce905da42c9e3bc21acd
tschmidt-stage <tschmidt@sacfoodcoop.com>
2012-08-10 09:55:33 -0700

    fixed submitted_by search

--- helpdesk/search_indexes.py
+++ helpdesk/search_indexes.py
@@ -8,18 +8,14 @@ class TicketIndex(indexes.RealTimeSearchIndex, indexes.Indexable):
     followup_str = indexes.CharField(model_attr='followup_str')
     title = indexes.CharField(model_attr='title')
     assigned_to = indexes.CharField(model_attr='assigned_to', null=True)
-    submitted_by = indexes.CharField(null=True)
+    submitted_by = indexes.CharField(model_attr='submitter_email', null=True)
     priority_str = indexes.CharField(model_attr='priority_str')
     status_str = indexes.CharField(model_attr='status_str')
     queue = indexes.CharField(model_attr='queue')
     milestone = indexes.CharField(model_attr='milestone', null=True)
     tags = indexes.MultiValueField(null=True)
-    #tags = indexes.CharField(null=True)
     tags_str = indexes.CharField(model_attr='tags_str', null=True)
 
-    #def prepare_tags(self, obj):
-    #    return [tag.name for tag in obj.tags.all()]
-        
     def get_model(self):
         return Ticket
 
